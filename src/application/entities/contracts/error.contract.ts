75dba981477a6cdc4022f5043b24ee6325005560
Danniel Ribeiro <danniel.ribeiro@gerencianet.com.br>
2017-12-11 11:23:04 -0200

    Added jsdoc description

--- src/application/entities/contracts/error.contract.ts
+++ src/application/entities/contracts/error.contract.ts
@@ -1,5 +1,8 @@
 import { ErrorType } from '../types/error-type';
 
+/**
+ *  Interface that describes error functions
+ */
 export interface ErrorContract {
   getError(error: ErrorType): Promise<string> | Promise<null>;
 }
